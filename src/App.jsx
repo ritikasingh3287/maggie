e51dc5f49d6ad9d3ee6297c191db0803a730fe85
Rana Pratap Singh <info@yogainstitute.com>
2026-04-14 15:38:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero'
+import Slider from './components/Slider'
+
+function App() {
+
+  return (
+    <>
+     <Hero />
+     <Slider />
+    </>
+  )
+}
+
+export default App
